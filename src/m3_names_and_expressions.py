19235fbccc967ad15f12f1622d0cd3f2561c6eb0
bundylc <bundy@rose-hulman.edu>
2018-08-30 16:51:30 -0400

    baseball

--- src/m3_names_and_expressions.py
+++ src/m3_names_and_expressions.py
@@ -3,7 +3,7 @@ answer = 2 ** 5
 print(answer * 100)
 
 ###############################################################################
-# TODO: 1.
+# DONE: 1.
 #   Read the 2 lines of code ABOVE this _TODO_.  That code:
 #     1. Computes 2 raised to the 5th power, yielding the object that is
 #          the integer 32.
@@ -20,8 +20,11 @@ print(answer * 100)
 ###############################################################################
 
 ###############################################################################
-# TODO: 2.
+# DONE: 2.
 #   Immediately below this _TODO_, write code that:
+import math
+answer=(77+math.cos(2.75))
+print(math.sqrt(answer))
 #     - Computes 77 plus the cosine of 2.75.
 #         HINT: You will need to import the   math  module (library).
 #     - Stores that computed value using a name of your own choosing.
@@ -30,22 +33,26 @@ print(answer * 100)
 ###############################################################################
 
 ###############################################################################
-# TODO: 3.
+# DONE: 3.
 #   Immediately below this _TODO_, write code that computes and prints:
 #      the square root of ((41 * 88) + (4 * the cosine of 2))
 #   Use as few or as many intermediate names as you feel appropriate.
 #   Run your code (fix errors as needed), then change the above _TODO_ to DONE.
+
+print(math.sqrt((41*88)+(4*math.cos(2))))
 ###############################################################################
 
 
 ###############################################################################
-# TODO: 4.
+# DONE: 4.
 #   Immediately below this _TODO_,
 #   write code that computes the square root of 2 in two ways:
 #     - By using the   math.sqrt   function.
 #     - By raising 2 to the 0.5 power (using   **   for exponentiation).
 #   Print both of the expressions that you write.
 #   Run your code (fix errors as needed), then change the above _TODO_ to DONE.
+print(math.sqrt(2))
+print(2 ** 0.5)
 ###############################################################################
 
 
@@ -67,7 +74,7 @@ print(answer * 100)
 #   will print     <class 'float'>
 #   Try it now!
 #   (Just write   print(type(3.14))   below this _TODO_ and run the program.)
-#
+print(type(3.14))
 #   Now go through the BLAH objects listed below.  For each:
 #      -- Try to GUESS its TYPE.
 #      -- Write code of the form   print(type(BLAH)).
